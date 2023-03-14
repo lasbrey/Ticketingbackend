1cddb258eca063bc01da27e6def6f8d76cc003bd
lasbrey lawal <lasbrey@macs-MacBook-Pro.local>
2023-03-14 17:40:52 +0100

    cors origin allow front-end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,11 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'https://ticket-management-system.herokuapp.com',
+    'http://localhost:5000',
+    'http://localhost:3000',
+    'https://ticketingfrontend-lasbrey.vercel.app/',
+    'https://ticketingfrontend-git-main-lasbrey.vercel.app/',
     'https://ticketingfrontend-topaz.vercel.app/',
-    'http://localhost:5000'
 ];
 
 module.exports = allowedOrigins;
